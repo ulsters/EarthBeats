defff8f2089560fb9062d2f9060091ed9387406a
ulsters <alkanmustafax@gmail.com>
2024-04-16 22:05:08 +0300

    app modules updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './ui/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: LandingComponent},
+    {path:"search" , loadChildren: () => import('./ui/search/search.module').then(module => module.SearchModule)},
+    {path:"news" , loadChildren: () => import('./ui/news/news.module').then(module => module.NewsModule)},
+    {path:"map" , loadChildren: () => import('./ui/map/map.module').then(module => module.MapModule)},
+    {path:"login" , loadChildren: () => import('./ui/auth/login/login.module').then(module => module.LoginModule)},
+    {path:"register" , loadChildren: () => import('./ui/auth/register/register.module').then(module => module.RegisterModule)},
+  ]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
