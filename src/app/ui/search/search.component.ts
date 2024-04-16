9edd39120b775880d2a67487faf0fe647cdb859a
ulsters <alkanmustafax@gmail.com>
2024-04-16 23:18:41 +0300

    axious added

--- src/app/ui/search/search.component.ts
+++ src/app/ui/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from "axios";
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+
+  private apiUrl = 'https://localhost:44343/api/Countries';
+ 
+  getJoke() {
+    axios.get(this.apiUrl)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
 }
