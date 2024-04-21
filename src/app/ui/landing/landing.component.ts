9dd11af38aa2dd7a631234bf5017a83d2fe0cd04
ulsters <alkanmustafax@gmail.com>
2024-04-21 19:13:50 +0300

    landing page routings added

--- src/app/ui/landing/landing.component.ts
+++ src/app/ui/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-landing',
@@ -6,6 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+
+  constructor(private router: Router) {} 
+
   handleCountryClick(event: MouseEvent) {
     const target = event.target as HTMLAreaElement;
     const countryName = target.getAttribute('data-country');
@@ -14,6 +19,10 @@ export class LandingComponent {
       // You can implement custom logic here, e.g., display information about the clicked country.
       console.log(`You clicked on ${countryName}`);
     }
+  }
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
   }
 
 }
