5e5dd53c8e73906fb983345194db1b8d116965ba
ulsters <alkanmustafax@gmail.com>
2024-04-27 17:35:07 +0300

    main page updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EarthBeats';
+
+  constructor(private router: Router) {} 
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
+
 }
