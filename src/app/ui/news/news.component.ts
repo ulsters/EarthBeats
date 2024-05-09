e34fd6b915fb843e084f6f92fa9af0073072682e
ulsters <alkanmustafax@gmail.com>
2024-05-09 23:49:22 +0300

    news page created and updated

--- src/app/ui/news/news.component.ts
+++ src/app/ui/news/news.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+interface NewsItem {
+  newsId: number;
+  newsTitle: string;
+  newsContent: string;
+  newsPublishDate: string;
+  newsImageUrl: string;
+}
 
 @Component({
   selector: 'app-news',
@@ -6,5 +18,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent {
+  
+  constructor(private router: Router) { }
+
+  navigateToNewsDetail(newsId: number): void {
+    // Navigate to the desired news detail URL with the specified newsId
+    this.router.navigate(['/news-detail', newsId]);
+  }
 
 }
