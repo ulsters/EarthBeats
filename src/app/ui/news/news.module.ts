06f401a27216784f657b018193f3cac77135f744
ulsters <alkanmustafax@gmail.com>
2024-05-10 01:26:25 +0300

    navbar and footer added to page

--- src/app/ui/news/news.module.ts
+++ src/app/ui/news/news.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsComponent } from './news.component';
 import { RouterModule } from '@angular/router';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 
 @NgModule({
@@ -10,6 +11,7 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
+    ComponentsModule,
     RouterModule.forChild([
       {path: '', component: NewsComponent}
     ])
