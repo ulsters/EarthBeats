e9f28af58e212ce9e43598b316b5b7f39eb8b913
ulsters <alkanmustafax@gmail.com>
2024-05-09 23:48:50 +0300

    responsiveness and module updates

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -7,6 +7,7 @@ import { MapModule } from './map/map.module';
 import { AuthModule } from './auth/auth.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NewsDetailModule } from './news-detail/news-detail.module';
 
 
 
@@ -20,14 +21,17 @@ import { HttpClientModule } from '@angular/common/http';
     MapModule,
     AuthModule,
     FormsModule,
+    NewsDetailModule,
     HttpClientModule
   ],
   exports:[
     LandingModule,
     NewsModule,
+    NewsDetailModule,
     SearchModule,
     MapModule,
-    AuthModule
+    AuthModule,
+
   ]
   
 })
