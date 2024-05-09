4f521744bcc38f16b67ef2eb03fe5f1ef0ff080c
ulsters <alkanmustafax@gmail.com>
2024-05-10 01:25:51 +0300

    navbar and footer added to page

--- src/app/ui/auth/login/login.module.ts
+++ src/app/ui/auth/login/login.module.ts
@@ -3,22 +3,25 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     CommonModule,
     HttpClientModule,
+    ComponentsModule,
+    RouterModule,
     RouterModule.forChild([
       {path: '', component: LoginComponent}
     ]),
   ],
   exports:[
-    LoginComponent
+    LoginComponent,  
   ]
 })
 export class LoginModule { }
